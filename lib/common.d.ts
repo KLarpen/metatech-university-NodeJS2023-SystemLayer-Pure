536c2e772b2eaf6d0a79e475461f2d4a74f948f7
Vladyslav Karpenko <klarpen@gmail.com>
2023-03-17 12:15:03 +0200

    Enhance password manipulation utilities
    Refactor password storing serialization format

--- lib/common.d.ts
+++ lib/common.d.ts
@@ -1,8 +1,10 @@
 declare namespace common {
   function hashPassword(password: string): Promise<string>;
+  function validatePassword(password: string, serHash: string): Promise<boolean>;
 }
 
 // declare function _exports(config: any): {
 //   hashPassword: (password: string) => Promise<string>;
+//   validatePassword: (password: string, serHash: string) => Promise<boolean>;
 // };
 // export = _exports;
