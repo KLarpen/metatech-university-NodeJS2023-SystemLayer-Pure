b210f5c0f74c9f533048b0a3c935fd200fb5e731
Vladyslav Karpenko <klarpen@gmail.com>
2023-06-19 08:22:24 +0300

    Add Node.js 20 support & update dependencies
    Apply prettier fix.

--- lib/common.d.ts
+++ lib/common.d.ts
@@ -1,6 +1,9 @@
 declare namespace common {
   function hashPassword(password: string): Promise<string>;
-  function validatePassword(password: string, serHash: string): Promise<boolean>;
+  function validatePassword(
+    password: string,
+    serHash: string,
+  ): Promise<boolean>;
 }
 
 // declare function _exports(config: any): {
