175a8d796877ac4a2f45ad7bc0368273c812254d
Vladyslav Karpenko <klarpen@gmail.com>
2023-06-21 22:00:01 +0300

    Changed db module export to separate CRUD repository generator and pg.Pool DB driver instance
    The change would make possible creation and instantiation
    of the domain specific repositories at the application level.

--- lib/db.d.ts
+++ lib/db.d.ts
@@ -1,11 +1,16 @@
+import type { Pool } from "pg";
+
 type QueryResult = Promise<object[]>;
 
-function deleteRecord(id: number | string): QueryResult;
+interface CrudRepository {
+  query(sql: string, args: any[]): QueryResult;
+  read(id: number | string, fields: string[]): QueryResult;
+  create(record: object): QueryResult;
+  update(id: number | string, record: object): QueryResult;
+  delete (id: number | string): QueryResult;
+}
 
 declare namespace db {
-  export function query(sql: string, args: any[]): QueryResult;
-  export function read(id: number | string, fields: string[]): QueryResult;
-  export function create(record: object): QueryResult;
-  export function update(id: number | string, record: object): QueryResult;
-  export { deleteRecord as delete };
+  export const pg: Pool;
+  export function crud(pgPool: Pool): (table: string) => CrudRepository;
 }
